a6a1d31008dad2257823ed2c79659a91e3175c37
Reshma <reshmashaik3011@gmail.com>
2022-05-15 00:52:21 +0530

    Removed test files & added 2 webhooks in index.js

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# github-bot
+### GitHub bot for spect.network
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) 
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Environment variables
+
+Go to [https://smee.io/](https://smee.io/) and start a new channel. Copy the Webhook Proxy URL, that's ```WEBHOOK_PROXY_URL```
+
+Navigate to [GitHub Developer Settings](https://github.com/settings/apps) and create an app. Create a webhook secret. You'll get the ```APP_ID``` & ```WEBHOOK_SECRET``` .
+
+Under [Personal Access Tokens](https://github.com/settings/tokens), you can generate a new Token to access the GitHub API. Save the ```.pem``` file in the working directory.
+
+## Documentation
+
+[Webhook Events and Payloads](https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads)
+
+[Octokit Webhooks](https://github.com/octokit/webhooks.js#webhook-events)
+
+[Probot](https://probot.github.io/docs/webhooks/)
+
+[Smee Client](https://github.com/probot/smee-client)
---
+++ index.js
@@ -0,0 +1,111 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+
+ const axios = require('axios');
+ const SmeeClient = require('smee-client')
+
+ const smee = new SmeeClient({
+   source: process.env.WEBHOOK_PROXY_URL,
+   target: 'http://localhost:4000/events',
+   logger: console
+ })
+ 
+ const events = smee.start()
+ events.close()
+
+
+module.exports = (app) => {
+  
+  app.log.info("Yay, the app has loaded!");
+  app.on('pull_request.opened', opened)
+  app.on('pull_request.closed', closed)
+
+
+  const server_url = process.env.MORALIS_SERVER_ID ;
+  const app_id = process.env.MORALIS_APPLICATION_ID;
+  let comment ;
+  
+  async function opened(context) {
+
+    const prhead = String(context.payload.pull_request.title).trim();
+    const keywords = prhead.split(' ',2);
+    const essential = keywords[0];
+    const taskId = keywords[1];
+
+    if (essential == "Spect"){
+
+      const name = String(context.payload.pull_request.user.login);
+      const prLink = String(context.payload.pull_request.html_url);
+
+      var data = {
+        "updates":{
+            "status": 200,
+        }
+      }
+
+      var config = {
+        method: 'get',
+        url: `${server_url}/functions/githubUpdateCard?_ApplicationId=${app_id}&taskId=${taskId}&link=${prLink}&user=${name}`,
+        data: data
+      };
+
+      await axios(config)
+      .then(function (response) {
+        console.log(response.data);
+        comment = "Hey "+name+", "+`${response.data.result}`;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      const issueComment = context.issue({
+        body: comment,
+      });
+      return context.octokit.issues.createComment(issueComment);
+    } 
+  }
+
+
+  async function closed(context) {
+
+    const mergeState = context.payload.pull_request.merged;
+    const prhead = String(context.payload.pull_request.title).trim();
+    const keywords = prhead.split(' ',2);
+    const essential = keywords[0];
+    const taskId = keywords[1];
+
+    if (essential == "Spect" & mergeState == true){
+
+      const prLink = String(context.payload.pull_request.html_url);
+      const name = String(context.payload.pull_request.user.login);
+
+      var data = {
+        "updates":{
+            "status": 205
+        }
+      }
+
+      var config = {
+        method: 'get',
+        url: `${server_url}/functions/githubUpdateCard?_ApplicationId=${app_id}&taskId=${taskId}&link=${prLink}&user=${name}`,
+        data: data
+      };
+
+      await axios(config)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    } 
+  }
+};
+ 
+
+
+
+
+
